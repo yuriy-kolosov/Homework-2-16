1a23fde595f1c656aa77d8723d56f178be36cbe7
yuriy-kolosov <yuriy.v.kolosov@gmail.com>
2024-05-09 06:23:21 +0300

    Домашнее задание 2-16. Основы алгоритмов. Часть 2. Задача

---
+++ src/main/java/pro/sky/homework216/exception/InvalidIndexException.java
@@ -0,0 +1,8 @@
+package pro.sky.homework216.exception;
+
+public class InvalidIndexException extends RuntimeException {
+
+    public InvalidIndexException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/pro/sky/homework216/exception/NoItemException.java
@@ -0,0 +1,7 @@
+package pro.sky.homework216.exception;
+
+public class NoItemException extends RuntimeException {
+    public NoItemException(String message) {
+        super(message);
+    }
+}
